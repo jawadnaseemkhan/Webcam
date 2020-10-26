71064e5fe218be7db08c74276319822dac80ba03
Jawad Naseem Khan <37769096+jawadnaseemkhan@users.noreply.github.com>
2020-10-26 18:14:51 +0500

    Add files via upload
    Webcam

---
+++ WebCam.py
@@ -0,0 +1,14 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _,frame = cap.read()
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(30)
+    if key ==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
